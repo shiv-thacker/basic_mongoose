1ebf803718af34a06c19bb55e8dececc5ec83282
shivang <18ec.shivangthacker@gmail.com>
2023-06-30 21:12:33 +0530

    create,insert document

--- src/app.js
+++ src/app.js
@@ -22,13 +22,33 @@ const playlistSchema = new mongoose.Schema({
   },
 });
 
+//mongoose is wrapper on schema
 // here we are making class of model, class starts with capital
-//It contains collection name (which should be in singular form, and first letter should capital) & schema
+//It contains collection name (which should be in singular form, and first letter should capital(Pascal form)) & schema
 const Playlist = new mongoose.model("Playlist", playlistSchema);
+
+//To create document we declare object reactPlaylist
+
+// reactPlaylist.save() is promies, means it takes some time & then gives output. So we will use async(), await for it & also we will use .then() .cache()
+
+const createDocument = async () => {
+  try {
+    const reactPlaylist = new Playlist({
+      name: "Node js",
+      ctype: "Back End",
+      videos: 50,
+      author: "shivang thacker",
+      active: true,
+    });
+    const result = await reactPlaylist.save();
+    console.log(result);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+createDocument();
 
 app.listen(8000, () => {
   console.log("on port 8000!!");
 });
-
-//mongoose is wrapper on schema
-// It provides an interface to the
