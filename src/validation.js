2ef4cbbcacca3d787213af0aebcfbed52285f1cc
shivang <18ec.shivangthacker@gmail.com>
2023-07-01 16:46:30 +0530

    builtin_validation

---
+++ src/validation.js
@@ -0,0 +1,67 @@
+// mongoose provide us validation with schema
+//"unique" is not a validator in mongoose, it helper for building MongoDB unique indexes. But it work as a valiadator.
+//trim can only trim befor first word blank space, and after last word blank space, it's not useful for middle space
+//match used to match values
+//minlength , maxlength, lowercase, uppercase
+//enum used to validate you variable, your input should be matching with one of it's values
+
+const mongoose = require("mongoose");
+
+const mongoDB = "mongodb://0.0.0.0:27017/mongoosebasic";
+
+mongoose
+  .connect(mongoDB)
+  .then(() => console.log("connection successfull"))
+  .catch((err) => console.log(`unable to connect server : ${err}`));
+
+//schema defines the data structure of document, also use to get values, validators etc., it also used to provide datatypes
+// here we create object or instance of schema
+const playlistSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    minlength: [2, "minimum 2 letters required"],
+    maxlength: 16,
+    enum: ["reactjs", "backend", "database"],
+  },
+  ctype: String,
+  videos: Number,
+  author: String,
+  active: Boolean,
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+//mongoose is wrapper on schema
+// here we are making class of model, class starts with capital
+//It contains collection name (which should be in singular form, and first letter should capital(Pascal form)) & schema
+const Playlist = new mongoose.model("Playlist", playlistSchema);
+
+//To create document we declare object reactPlaylist
+
+// reactPlaylist.save() is promies, means it takes some time & then gives output. So we will use async(), await for it & also we will use .then() .cache()
+
+const createDocument = async () => {
+  try {
+    //to pass multiple data we just need to add one more object
+    const mongoPlaylist = new Playlist({
+      name: "reactjs",
+      ctype: "database",
+      videos: 10,
+      author: "shivang thacker",
+      active: true,
+    });
+    // const result = await reactPlaylist.save(); to insert only one variable
+    const result = await Playlist.insertMany([mongoPlaylist]);
+    console.log(result);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+createDocument();
